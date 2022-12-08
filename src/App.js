aabeccf5ba78460bc92483c82153bcf90442974b
patedwards <pat.edwards@pm.me>
2022-12-08 13:58:43 -0800

    works

--- src/App.js
+++ src/App.js
@@ -1,278 +1,218 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DeckGL from "@deck.gl/react";
 import { load } from "@loaders.gl/core";
+import { BitmapLayer } from "@deck.gl/layers";
 import { TileLayer } from "@deck.gl/geo-layers";
-import Drawer from "@mui/material/Drawer";
-import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import Selector from "./Selector";
-import { BitmapLayer } from 'deck.gl';
+import Drawer from "@mui/material/Drawer";
 
+function generateColor(value) {
+  // Make sure value is in range [0, 1]
+  //value = Math.max(0, Math.min(1, value));
+  // Map value to color using a spectral ramp
+  let r, g, b;
+  if (value < 0.25) {
+    r = 0;
+    g = 4 * value;
+    b = 1;
+  } else if (value < 0.5) {
+    r = 0;
+    g = 1;
+    b = 1 - 4 * (value - 0.25);
+  } else if (value < 0.75) {
+    r = 4 * (value - 0.5);
+    g = 1;
+    b = 0;
+  } else {
+    r = 1;
+    g = 1 - 4 * (value - 0.75);
+    b = 0;
+  }
+  // Return color as hexadecimal string
+  //return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+  return [r * 255, g * 255, b * 255];
+}
 
-const COLOR_MAPS = [
-  "magma",
-  "inferno",
-  "plasma",
-  "viridis",
-  "cividis",
-  "twilight",
-  "twilight_shifted",
-  "turbo",
-  "Blues",
-  "BrBG",
-  "BuGn",
-  "BuPu",
-  "CMRmap",
-  "GnBu",
-  "Greens",
-  "Greys",
-  "OrRd",
-  "Oranges",
-  "PRGn",
-  "PiYG",
-  "PuBu",
-  "PuBuGn",
-  "PuOr",
-  "PuRd",
-  "Purples",
-  "RdBu",
-  "RdGy",
-  "RdPu",
-  "RdYlBu",
-  "RdYlGn",
-  "Reds",
-  "Spectral",
-  "Wistia",
-  "YlGn",
-  "YlGnBu",
-  "YlOrBr",
-  "YlOrRd",
-  "afmhot",
-  "autumn",
-  "binary",
-  "bone",
-  "brg",
-  "bwr",
-  "cool",
-  "coolwarm",
-  "copper",
-  "cubehelix",
-  "flag",
-  "gist_earth",
-  "gist_gray",
-  "gist_heat",
-  "gist_ncar",
-  "gist_rainbow",
-  "gist_stern",
-  "gist_yarg",
-  "gnuplot",
-  "gnuplot2",
-  "gray",
-  "hot",
-  "hsv",
-  "jet",
-  "nipy_spectral",
-  "ocean",
-  "pink",
-  "prism",
-  "rainbow",
-  "seismic",
-  "spring",
-  "summer",
-  "terrain",
-  "winter",
-  "Accent",
-  "Dark2",
-  "Paired",
-  "Pastel1",
-  "Pastel2",
-  "Set1",
-  "Set2",
-  "Set3",
-  "tab10",
-  "tab20",
-  "tab20b",
-  "tab20c",
-  "magma_r",
-  "inferno_r",
-  "plasma_r",
-  "viridis_r",
-  "cividis_r",
-  "twilight_r",
-  "twilight_shifted_r",
-  "turbo_r",
-  "Blues_r",
-  "BrBG_r",
-  "BuGn_r",
-  "BuPu_r",
-  "CMRmap_r",
-  "GnBu_r",
-  "Greens_r",
-  "Greys_r",
-  "OrRd_r",
-  "Oranges_r",
-  "PRGn_r",
-  "PiYG_r",
-  "PuBu_r",
-  "PuBuGn_r",
-  "PuOr_r",
-  "PuRd_r",
-  "Purples_r",
-  "RdBu_r",
-  "RdGy_r",
-  "RdPu_r",
-  "RdYlBu_r",
-  "RdYlGn_r",
-  "Reds_r",
-  "Spectral_r",
-  "Wistia_r",
-  "YlGn_r",
-  "YlGnBu_r",
-  "YlOrBr_r",
-  "YlOrRd_r",
-  "afmhot_r",
-  "autumn_r",
-  "binary_r",
-  "bone_r",
-  "brg_r",
-  "bwr_r",
-  "cool_r",
-  "coolwarm_r",
-  "copper_r",
-  "cubehelix_r",
-  "flag_r",
-  "gist_earth_r",
-  "gist_gray_r",
-  "gist_heat_r",
-  "gist_ncar_r",
-  "gist_rainbow_r",
-  "gist_stern_r",
-  "gist_yarg_r",
-  "gnuplot_r",
-  "gnuplot2_r",
-  "gray_r",
-  "hot_r",
-  "hsv_r",
-  "jet_r",
-  "nipy_spectral_r",
-  "ocean_r",
-  "pink_r",
-  "prism_r",
-  "rainbow_r",
-  "seismic_r",
-  "spring_r",
-  "summer_r",
-  "terrain_r",
-  "winter_r",
-  "Accent_r",
-  "Dark2_r",
-  "Paired_r",
-  "Pastel1_r",
-  "Pastel2_r",
-  "Set1_r",
-  "Set2_r",
-  "Set3_r",
-  "tab10_r",
-  "tab20_r",
-  "tab20b_r",
-  "tab20c_r"
-];
+function evaluateMandelbrot(x0, y0, maxIterations) {
+  let x = 0.0;
+  let y = 0.0;
+  // Perform Mandelbrot set iterations
+  for (let iteration = 0; iteration < maxIterations; iteration++) {
+    let xNew = x * x - y * y + x0;
+    y = 2 * x * y + y0;
+    x = xNew;
+
+    // If escaped
+    if (x * x + y * y > 4.0) {
+      // Color using pretty linear gradient
+      let color = 1 - 0.01 * (iteration - Math.log2(Math.log2(x * x + y * y)));
+      return color;
+    }
+  }
+  // If failed, set color to black
+  return 0;
+}
+
+function makeMandelbrot(x, y, zoom, scale, maxIterations) {
+  // Calculate values to make navigation easier
+  const zF = 2 ** zoom;
+  const xFrom = x / zF;
+  const xTo = (1 + x) / zF;
+  const yFrom = y / zF;
+  const yTo = (1 + y) / zF;
+  const height = 256;
+  const width = 256;
+
+  const xs = (xTo - xFrom) / width;
+  const ys = (yTo - yFrom) / height;
+
+  const image = [];
+  for (let j = 0; j < 256; j++) {
+    let row = [];
+    for (let i = 0; i < 256; i++) {
+      let x0 = i * xs + xFrom;
+      let y0 = j * ys + yFrom;
+      let value = scale * evaluateMandelbrot(x0, y0, maxIterations);
+      let pixel = generateColor(value);
+      row.push(pixel);
+    }
+    image.push(row);
+  }
+
+  return image;
+}
 
 function App() {
   const [viewState, setViewState] = useState({
-    longitude: -2.8125,
-    latitude: 1.9167318494636127,
-    zoom: 6
+    longitude: 0,
+    latitude: 0,
+    zoom: 0
   });
 
-  const [scale, setScale] = useState(-6);
-  const [iterations, setIterations] = useState(500);
-  const [cmap, setCmap] = useState("turbo");
-  const [redFactor, setRedFactor] = useState(1)
+  const [scale, setScale] = useState(1);
+  const [maxIterations, setMaxIterations] = useState(100);
 
-  const saveState = () => {
-    fetch(
-      `http://127.0.0.1:8890/save?latitude=${viewState.latitude}&longitude=${viewState.longitude}&width=${viewState.width}&height=${viewState.height}&scale=${scale}&maxIterations=${iterations}&cmap=${cmap}&zoom=${viewState.zoom}`
-    ).then(r => console.log(r));
-  };
+  const getScale = () => scale
+  useEffect(() => {}, [scale, maxIterations])
+
+  const SCALES = {
+    0: 1,
+    1: 1,
+    2: 1,
+    3: 1,
+    4: 1,
+    5: 1,
+    6: 1,
+    7: 1,
+    8: 1,
+    9: 1,
+    10: 1,
+    11: 1,
+    12: 1,
+    13: 1,
+    14: 1,
+    15: 1,
+    16: 1,
+    17: 1,
+    18: -1,
+    19: -1,
+    20: -1,
+    21: -2,
+    22: -2,
+    23: -2,
+    24: -2,
+    25: -6,
+    26: -6,
+    27: -2,
+    28: -2,
+    29: -2,
+    30: -2,
+    31: -2,
+    32: -2,
+    33: -3,
+    34: -3,
+    35: -3,
+    36: -3,
+  }
 
   const layer = new TileLayer({
     // https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Tile_servers
-    data: `http://127.0.0.1:8899/{z}/{x}/{y}?scale=${scale}&maxIterations=${iterations}&cmap=${cmap}`,
     minZoom: 0,
     maxZoom: Infinity,
     tileSize: 256,
-    getTileData: async tile => {
-      return load(tile.url)
+    getTileData: ({ x, y, z }) => {
+      const s = SCALES[z]
+      const it = Math.min(100*(z), 2000)
+      console.log(z, it, s)
+      const pixels = makeMandelbrot(x, y, z, s, it);
+      // Create a canvas element
+      const canvas = document.createElement("canvas");
+      // Set the dimensions of the canvas
+      canvas.width = pixels[0].length;
+      canvas.height = pixels.length;
+
+      // Get the 2D rendering context of the canvas
+      const ctx = canvas.getContext("2d");
+
+      // Create a new ImageData object
+      const imageData = new ImageData(pixels[0].length, pixels.length);
+
+      // Set the pixel data of the ImageData object
+      for (let i = 0; i < pixels.length; i++) {
+        for (let j = 0; j < pixels[i].length; j++) {
+          const index = (i * pixels[i].length + j) * 4;
+          imageData.data[index + 0] = pixels[i][j][0]; // Red channel
+          imageData.data[index + 1] = pixels[i][j][1]; // Green channel
+          imageData.data[index + 2] = pixels[i][j][2]; // Blue channel
+          imageData.data[index + 3] = 255; // Alpha channel
+        }
+      }
+
+      // Draw the image data on the canvas
+      ctx.putImageData(imageData, 0, 0);
+
+      return imageData;
     },
 
     renderSubLayers: props => {
       const {
-        bbox: { west, south, east, north },
+        bbox: { west, south, east, north }
       } = props.tile;
-      
       return new BitmapLayer(props, {
         // can maybe load the image from an array see https://loaders.gl/docs/specifications/category-image
         // this means API would return an array not an image
+        //tintColor: (r, g, b) => [r/255, 0, 0, 0, 0, g/255, 0, 0, 0, 0, b/255, 0, 0, 0, 0, 1],
         data: null,
         image: props.data,
-        bounds: [west, south, east, north],
-        colorMap: [redFactor, 1, 1]
+        bounds: [west, south, east, north]
       });
     }
   });
-
   return (
     <div>
       <Drawer anchor={"right"} open={true} variant="permanent">
-        <TextField
-          value={scale}
-          onChange={event => setScale(event.target.value)}
-        />
-        <TextField
-          value={iterations}
-          onChange={event => setIterations(event.target.value)}
-        />
-        <TextField
-          value={redFactor}
-          onChange={event => setRedFactor(event.target.value)}
-        />
-        <Selector
-          items={COLOR_MAPS.map(c => ({ label: c }))}
-          handleSelection={item => setCmap(item.label)}
-        />
+      <TextField
+        value={scale}
+        onChange={event => setScale(event.target.value)}
+      />
+      <TextField
+        value={maxIterations}
+        onChange={event => setMaxIterations(event.target.value)}
+      />
       </Drawer>
-
       <DeckGL
         controller={true}
         initialViewState={{
-          latitude: -30.33432259963514,
-            longitude: -124.46095039521886,
-            zoom: 1,
+          longitude: 0,
+          latitude: 0,
+          zoom: 2,
           maxZoom: Infinity
         }}
-        onViewStateChange= {({viewState}) => {
-          setViewState(viewState)
-          }
-        }
+        onViewStateChange={setViewState}
         layers={[layer]}
       />
-      <Box
-        style={{
-          zIndex: 10000,
-          position: "absolute",
-          top: 16,
-          left: 16,
-          width: 256
-        }}
-      >
-        <Button onClick={() => saveState()}>Save</Button>
-      </Box>
     </div>
   );
 }
 
 export default App;
-//7 1.40625 0.8398268729947986
