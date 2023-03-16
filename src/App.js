904b2a2158e5620bc4f0a111aaf6a5c21e6a5969
patedwards <pat.edwards@pm.me>
2023-03-15 19:17:56 -0700

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 import { ThemeProvider } from "@mui/material/styles";
-import LibraryIcon from '@mui/icons-material/PhotoLibrary';
 import PaletteIcon from "@mui/icons-material/Palette";
-import SaveIcon from '@mui/icons-material/BookmarkAdd';
-import EditLocationIcon from '@mui/icons-material/EditLocation';
-import ScreenshotMonitorIcon from '@mui/icons-material/ScreenshotMonitor';
-import EditParametersIcon from '@mui/icons-material/Functions';
+import EditLocationIcon from "@mui/icons-material/EditLocation";
+import EditParametersIcon from "@mui/icons-material/Functions";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 import { Stack, Box } from "@mui/material";
 
 import theme from "./Theme";
 
-import Library from "./components/Library";
 import Map from "./components/Map";
 import AppBar from "./components/AppBar";
-import CoordinateSetter from "./components/Form"
-import GradientStyler from "./components/GradientStyler"
+import CoordinateSetter from "./components/Form";
+import GradientStyler from "./components/GradientStyler";
 import TaskDrawer from "./components/TaskDrawer";
 
-import { captureImage } from "./utilities/deck";
-import { zToLatLon, latLongToZ } from "./utilities/math"
-
-import { useLibraryLU } from "./hooks/library";
-
-const includedViews = [
-
-]
+import { zToLatLon, latLongToZ } from "./utilities/math";
 
 const taskNames = {
   coordinateSetter: "COORDINATE_SETTER",
   styleSetter: "STYLE_SETTER",
-  parametersSetter: "PARAMETERS_SETTER"
-}
-
+  parametersSetter: "PARAMETERS_SETTER",
+};
 
 /*
 
@@ -58,7 +46,7 @@ function App() {
   const [colors, setColors] = useState({
     start: { r: 35, g: 44, b: 51, hex: "#232C33" },
     middle: { r: 219, g: 62, b: 0, hex: "#db3e00" },
-    end: { r: 83, g: 0, b: 235, hex: "#5300eb" }
+    end: { r: 83, g: 0, b: 235, hex: "#5300eb" },
   });
   // Map state
   const [viewState, setViewState] = useState({
@@ -66,148 +54,90 @@ function App() {
     latitude: 0,
     zoom: 2,
     minZoom: 2,
-    maxZoom: Infinity
+    maxZoom: Infinity,
   });
-  const [z, setZ] = useState({ x: 0, y: 0 })
+  const [z, setZ] = useState({ x: 0, y: 0 });
   // Component toggling
-  const [showCoordinateSetter, setShowCoordinateSetter] = useState(false)
-  const [stylebarOpen, setStylebarOpen] = useState(false)
-  const [libraryOpen, setLibraryOpen] = useState(false)
-  const [activeTask, setActiveTask] = useState(null)
+  const [activeTask, setActiveTask] = useState(null);
 
-  // Meta-application state
-  const [savedViews, updateSavedViews, removeItem] = useLibraryLU()
-
-  // event handlers
-  const handleScreenshot = async () => {
-    const el = await captureImage({
-      viewState,
-      maxIterations,
-      colors,
-      gradientFunction, ratio: 4
-    }
-    )
-    el.download = "output.png";
-    document.body.appendChild(el);
-    const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
-    await sleep(5000)
-    el.click();
-    document.body.removeChild(el);
-
-  }
-
-  const handleSaveView = async () => {
-    const el = await captureImage({
-      viewState, ratio: 0.2,
-      maxIterations,
-      colors,
-      gradientFunction
-    })
-
-    const imgPath = `${uuidv4()}.png`
-
-    const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
-    await sleep(5000)
-    window.localStorage.setItem(imgPath, el)
-
-
-    const view = {
-      viewState, imgPath, colors, maxIterations, gradientFunction
-    }
-
-    updateSavedViews(view)
-
-  }
-
-  const handleLoadView = (view) => {
-    console.log("view", view)
-    setViewState((({ latitude, longitude, zoom, bearing, pitch }) => ({ latitude, longitude, zoom, bearing, pitch }))(view.viewState))
-    setColors(view.colors)
-    setMaxIterations(view.maxIterations)
-    setGradientFunction(view.gradientFunction)
-  }
+  const isScreenWidthLessThan400 = useMediaQuery("(max-width:400px)");
 
   // Available tools
   const tools = {
     xyToggle: {
       label: "Set coordinates",
       onClick: () => setActiveTask(taskNames.coordinateSetter),
-      icon: EditLocationIcon
-    },
-    libraryToggle: {
-      label: "library",
-      onClick: () => setLibraryOpen(!libraryOpen),
-      icon: LibraryIcon
+      icon: EditLocationIcon,
     },
     parametersToggle: {
       label: "parameters",
       onClick: () => setActiveTask(taskNames.parametersSetter),
-      icon: EditParametersIcon
+      icon: EditParametersIcon,
     },
     styleToggle: {
       label: "style",
       onClick: () => setActiveTask(taskNames.styleSetter),
-      icon: PaletteIcon
+      icon: PaletteIcon,
     },
-    saveToggle: {
-      label: "save",
-      onClick: handleSaveView,
-      icon: SaveIcon
-    },
-    captureToggle: {
-      label: "capture",
-      onClick: handleScreenshot,
-      icon: ScreenshotMonitorIcon
-    },
-    
-  }
+  };
 
   // Activity configuration
 
-  const editTools = [tools.xyToggle, tools.parametersToggle, tools.styleToggle]
-  const shareSaveTools = [tools.libraryToggle, tools.saveToggle, tools.captureToggle]
+  const editTools = [tools.xyToggle, tools.parametersToggle, tools.styleToggle];
 
   // TODO: put this in a State and use useEffect to update step size and initial value for max its based on Zoom value
   // TODO: make the setViewState work
   const setZActivity = {
     tools: [
       {
-        label: "X", inputType: "number", initialValue: z.x, inputProps: { step: 1 / 2 ** (viewState.zoom + 1) }
+        label: "X",
+        inputType: "number",
+        initialValue: z.x,
+        inputProps: { step: 1 / 2 ** (viewState.zoom + 1) },
       },
       {
-        label: "Y", inputType: "number", initialValue: z.y, inputProps: { step: 1 / 2 ** (viewState.zoom + 1) }
+        label: "Y",
+        inputType: "number",
+        initialValue: z.y,
+        inputProps: { step: 1 / 2 ** (viewState.zoom + 1) },
       },
       {
-        label: "Zoom", inputType: "number", initialValue: viewState.zoom, inputProps: { step: 1 }
+        label: "Zoom",
+        inputType: "number",
+        initialValue: viewState.zoom,
+        inputProps: { step: 1 },
       },
-    ]
-  }
+    ],
+  };
 
-  const setZActivityFormSubmit = formState => {
-    console.log("formState", formState)
+  const setZActivityFormSubmit = (formState) => {
+    console.log("formState", formState);
     setViewState({
       ...viewState,
       ...zToLatLon({ x: formState["X"], y: formState["Y"] }),
       zoom: formState["Zoom"],
-    })
-    setMaxIterations(formState["Number of iterations"])
-  }
+    });
+    setMaxIterations(formState["Number of iterations"]);
+  };
 
   const parametersActivity = {
     tools: [
-      { label: "Number of iterations", initialValue: maxIterations, inputType: "number", inputProps: { step: 100 } }
-    ]
-  }
+      {
+        label: "Number of iterations",
+        initialValue: maxIterations,
+        inputType: "number",
+        inputProps: { step: 100 },
+      },
+    ],
+  };
 
-  const setParametersFormSubmit = formState => {
-    setMaxIterations(formState["Number of iterations"])
-  }
+  const setParametersFormSubmit = (formState) => {
+    setMaxIterations(formState["Number of iterations"]);
+  };
 
   useEffect(() => {
-
-    setZ({ ...latLongToZ(viewState), ...viewState, })
-
-  }, [viewState])
+    setZ({ ...latLongToZ(viewState), ...viewState });
+  }, [viewState]);
 
   /*
   Careful about cycling dependency here
@@ -218,21 +148,48 @@ function App() {
   }, [z])
   */
 
-
   //
   return (
     <div>
-      <TaskDrawer editTools={editTools} shareSaveTools={shareSaveTools} />
-      <AppBar />
-      <Box sx={{ position: "absolute", top: 80, left: 80, width: 360 }}>
-        <Stack spacing={2}>
-          {activeTask === taskNames.coordinateSetter ? <CoordinateSetter {...{ ...setZActivity, formSubmit: setZActivityFormSubmit }} /> : null}
-          {activeTask === taskNames.styleSetter ? <GradientStyler {...{ colors, setColors, setGradientFunction }} /> : null}
-          {activeTask === taskNames.parametersSetter ? <CoordinateSetter {...{ ...parametersActivity,  formSubmit: setParametersFormSubmit }} /> : null}
-        </Stack>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: isScreenWidthLessThan400 ? "column-reverse" : "row",
+          minHeight: "100vh",
+        }}
+      >
+        <TaskDrawer editTools={editTools} />
+        <AppBar />
+        <Box sx={{ position: "absolute", top: 80, left: 80, width: 360 }}>
+          <Stack spacing={2}>
+            {activeTask === taskNames.coordinateSetter ? (
+              <CoordinateSetter
+                {...{ ...setZActivity, formSubmit: setZActivityFormSubmit }}
+              />
+            ) : null}
+            {activeTask === taskNames.styleSetter ? (
+              <GradientStyler {...{ colors, setColors, setGradientFunction }} />
+            ) : null}
+            {activeTask === taskNames.parametersSetter ? (
+              <CoordinateSetter
+                {...{
+                  ...parametersActivity,
+                  formSubmit: setParametersFormSubmit,
+                }}
+              />
+            ) : null}
+          </Stack>
+        </Box>
+        <Map
+          {...{
+            maxIterations,
+            colors,
+            gradientFunction,
+            setViewState,
+            initialViewState: viewState,
+          }}
+        />
       </Box>
-      <Map {...{ maxIterations, colors, gradientFunction, setViewState, initialViewState: viewState }} />
-      <Library {...{ libraryOpen, setLibraryOpen, savedViews, includedViews, handleLoadView, removeItem }} />
     </div>
   );
 }
@@ -246,4 +203,3 @@ const RootApp = () => {
 };
 
 export default RootApp;
-
--- src/components/TaskDrawer.js
+++ src/components/TaskDrawer.js
@@ -1,113 +1,121 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import List from "@mui/material/List";
+import { Toolbar } from "@mui/material";
+import Box from "@mui/material/Box";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
-const drawerWidth = 240;
+const drawerWidth = 48;
 
 const openedMixin = (theme) => ({
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: 'hidden',
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme) => ({
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up('sm')]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
-    },
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
 });
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-        boxSizing: 'border-box',
-        ...(open && {
-            ...openedMixin(theme),
-            '& .MuiDrawer-paper': openedMixin(theme),
-        }),
-        ...(!open && {
-            ...closedMixin(theme),
-            '& .MuiDrawer-paper': closedMixin(theme),
-        }),
-    }),
-);
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 
-const Tool = tool => {
-    return <ListItem key={tool.label} disablePadding sx={{ display: 'block' }}>
-        <ListItemButton
-            {...tool}
-            sx={{
-                minHeight: 48,
-                justifyContent: 'initial',
-                px: 2.5,
-            }}
+const Tool = (tool) => {
+  return (
+    <ListItem key={tool.label} disablePadding sx={{ display: "block" }}>
+      <ListItemButton
+        {...tool}
+        sx={{
+          minHeight: 48,
+          justifyContent: "initial",
+          px: 2.5,
+        }}
+      >
+        <ListItemIcon
+          sx={{
+            minWidth: 0,
+            mr: 3,
+            justifyContent: "center",
+          }}
         >
-            <ListItemIcon
-                sx={{
-                    minWidth: 0,
-                    mr: 3,
-                    justifyContent: 'center',
-                }}
-            >
-                <tool.icon />
-            </ListItemIcon>
-            <ListItemText primary={tool.label} sx={{ opacity: 1 }} />
-        </ListItemButton>
+          <tool.icon />
+        </ListItemIcon>
+      </ListItemButton>
     </ListItem>
-}
+  );
+};
 
+// The TaskDrawer component is a drawer that contains the tools for editing the Mandelbrot set.
+// It is a list of buttons that can be clicked to change the Mandelbrot set.
+// It sits on the left side of the screen when the screen is wide enough, and on the bottom when the screen is narrow.
+export default function MiniDrawer({ editTools }) {
+  const theme = useTheme();
+  const isScreenWidthLessThan400 = useMediaQuery("(max-width:400px)");
 
-export default function MiniDrawer({ editTools, shareSaveTools }) {
-    const theme = useTheme();
-
-    return (
-        <MuiAppBar>
-            <Drawer variant="permanent" >
-                <DrawerHeader>
-                    <IconButton>
-                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-                    </IconButton>
-                </DrawerHeader>
-                <Divider />
-                <List>
-                    {editTools.map(tool => (
-                        <Tool {...tool} />
-                    ))}
-                    <Divider />
-                    {shareSaveTools.map(tool => (
-                        <Tool {...tool} />
-                    ))}
-                </List>
-            </Drawer>
-        </MuiAppBar>
-    );
+  return (
+    <MuiAppBar>
+      <Toolbar />
+      <MuiDrawer
+        variant="permanent"
+        anchor={isScreenWidthLessThan400 ? "bottom" : "left"}
+        open={true}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: isScreenWidthLessThan400 ? "row" : "column",
+            width: isScreenWidthLessThan400 ? "100%" : 60,
+            height: isScreenWidthLessThan400 ? 48 : "100%",
+            alignItems: isScreenWidthLessThan400 ? "center" : "center",
+            justifyContent: isScreenWidthLessThan400 ? "center" : "left",
+            paddingTop: isScreenWidthLessThan400 ? 0 : 8,
+          }}
+        >
+          {editTools.map((tool) => (
+            <Tool {...tool} />
+          ))}
+        </Box>
+      </MuiDrawer>
+    </MuiAppBar>
+  );
 }
