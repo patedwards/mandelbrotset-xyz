557ad2cb97aca23d432fb6ca369ce2084d77205a
patedwards <pat.edwards@pm.me>
2023-08-07 06:07:01 -0700

    Fixed the math

--- src/utilities/math.js
+++ src/utilities/math.js
@@ -25,12 +25,3 @@ export const gradientFunctions = {
     1 -  (iteration / maxIterations - Math.log2(Math.log2(x * x + y * y)))
 };
 
-export const zToLatLon = z => {
-  // The map will cover -1 to 1 in both dimensions
-  return { longitude: 180 * z.x, latitude: 90 * z.y }
-}
-
-export const latLongToZ = ({ latitude, longitude }) => {
-  // The map will cover -1 to 1 in both dimensions
-  return { x: longitude/180, y: latitude/90 }
-}
--- src/utilities/tileGeneration.js
+++ src/utilities/tileGeneration.js
@@ -1,29 +1,41 @@
-import {evaluateMandelbrot } from "./math";
+import { evaluateMandelbrot } from "./math";
 
 export function makeMandelbrot(x, y, zoom, maxIterations) {
-    // Calculate values to make navigation easier
-    const zF = 2 ** zoom;
-    const xFrom = x / zF;
-    const xTo = (1 + x) / zF;
-    const yFrom = y / zF;
-    const yTo = (1 + y) / zF;
-    const height = 256;
-    const width = 256;
-  
-    const xs = (xTo - xFrom) / width;
-    const ys = (yTo - yFrom) / height;
-    const image = [];
-    for (let j = 0; j < 256; j++) {
-      let row = [];
-      for (let i = 0; i < 256; i++) {
-        let x0 = i * xs + xFrom;
-        let y0 = j * ys + yFrom;
-        let [x, y, iterations] = evaluateMandelbrot(x0, y0, maxIterations);
-        
-        row.push([x, y, iterations]);
-      }
-      image.push(row);
+  const TILE_SIZE = 256;
+  const height = TILE_SIZE;
+  const width = TILE_SIZE;
+
+  const lonFrom = tile2lon(x, zoom);
+  const lonTo = tile2lon(x + 1, zoom);
+  const latFrom = tile2lat(y + 1, zoom);
+  const latTo = tile2lat(y, zoom);
+
+  const lonStep = (lonTo - lonFrom) / width;
+  const latStep = (latTo - latFrom) / height;
+
+  const image = [];
+  for (let j = 0; j < height; j++) {
+    let row = [];
+    for (let i = 0; i < width; i++) {
+      let x0 = i * lonStep + lonFrom;
+      let y0 = (height - 1 - j) * latStep + latFrom; // Inverting j here
+
+      let [x, y, iterations] = evaluateMandelbrot(x0, y0, maxIterations);
+      row.push([x, y, iterations]);
     }
-  
-    return image;
+    image.push(row);
   }
+
+  return image;
+}
+
+// Convert tile coordinate to longitude
+function tile2lon(x, z) {
+  return (x / Math.pow(2, z)) * 360 - 180;
+}
+
+// Convert tile coordinate to latitude
+function tile2lat(y, z) {
+  let n = Math.PI - (2 * Math.PI * y) / Math.pow(2, z);
+  return (180 / Math.PI) * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n)));
+}
